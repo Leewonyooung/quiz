6d3b9dcf03640d96df038b7249c1f253b63e4d1a
wonyoung <wylee99@naver.com>
2024-03-17 16:00:08 +0900

    Ready for deploy

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:Test1/screen/screen_home.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Quiz App',
+      home: HomeScreen(),
+    );
+  }
+}
